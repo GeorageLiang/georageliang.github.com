dda6454c27cfd8134dec242d261726a1fcd6a67e
GeorageLiang <callmelianghaoxu@gmail.com>
2015-06-19 18:30:38 +0800

    update

--- js/geo.js
+++ js/geo.js
@@ -8,4 +8,23 @@ $(document).ready(function(){
       "unpinned": "down"
     }
   });
+  setInterval(function(){                //每2秒钟执行一次
+
+  $("#cat-head").css("-webkit-transform","rotate(-10deg)");
+  //id为head的元素（即猫头）实现向右旋转10度（chrome内核）
+  $("#cat-head").css("-o-transform","rotate(-10deg)");
+  //id为head的元素（即猫头）实现向右旋转10度（opear内核）
+  $("#cat-head").css("-moz-transform","rotate(-10deg)");
+  //id为head的元素（即猫头）实现向右旋转10度（火狐内核）
+  setTimeout(function(){
+
+    $("#cat-head").css("-webkit-transform","rotate(10deg)");  //一秒后执行猫头向左旋转10度
+    $("#cat-head").css("-o-transform","rotate(10deg)");
+    $("#cat-head").css("-moz-transform","rotate(10deg)");
+
+  },1000);
+
+},2000);
+
+
 });
