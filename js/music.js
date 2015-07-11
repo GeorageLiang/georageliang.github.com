cd1793b84ee01cb3bf656f1d9e49c68ffb8bcc4a
GeorageLiang <callmelianghaoxu@gmail.com>
2015-07-11 11:02:04 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -6,7 +6,7 @@ KISSY.ready(function(S){
 
   KISSY.use('gallery/musicPlayer/2.0/index', function (S, MusicPlayer) {
     musicPlayer = new MusicPlayer({
-      auto:true, //自动播放 默认不播放.
+      auto:false, //自动播放 默认不播放.
       mode:'random', //如果几首歌想随机播放,设置为 random, 默认为order.
       musicList:[{"name":"quiet", "path":"http://shop.cngame.com/res/2007Tuesday134750.mp3"}],
       volume:0.8
